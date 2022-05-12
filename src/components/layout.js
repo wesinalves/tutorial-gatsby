f271d5e630675ddfa25466c50d140d26e214c065
Wesin Ribeiro <wesinec@gmail.com>
2022-05-12 17:31:46 -0300

    testing breakpoints

--- src/components/layout.js
+++ src/components/layout.js
@@ -27,6 +27,21 @@ import MenuItem from '@mui/material/MenuItem';
 import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
 import IconButton from '@mui/material/IconButton';
+import { styled } from '@mui/material/styles';
+
+
+const Root = styled('div')(({ theme }) => ({
+  padding: theme.spacing(1),
+  [theme.breakpoints.down('md')]: {
+    backgroundColor: red[500],
+  },
+  [theme.breakpoints.up('md')]: {
+    backgroundColor: grey[500],
+  },
+  [theme.breakpoints.up('lg')]: {
+    backgroundColor: green[500],
+  },
+}));
 
 
 function ScrollTop(props) {
@@ -239,6 +254,12 @@ const Layout = ({props, pageTitle, children}) => {
             <main>                
                 {children}
             </main>
+
+            <Root>
+              <Typography>down(md): red</Typography>
+              <Typography>up(md): blue</Typography>
+              <Typography>up(lg): green</Typography>
+            </Root>
             {/* Footer */}
             <Container
                 maxWidth="md"

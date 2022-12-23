30564b309e42e39dcd4b15b1d1f6aac63f70de74
Wesin Ribeiro Alves <wesinec@gmail.com>
2022-12-23 06:32:15 -0300

    update policy and terms

--- src/pages/policy.js
+++ src/pages/policy.js
@@ -17,8 +17,8 @@ const PolicyPage = () => {
                 <Header name='Política de Privacidade' links={[{path: '/', description:'Home'}]}/>                
                 <Box>
                 <h2>Privacidade</h2>
-                <p>A sua privacidade é importante para nós. É política do wesinribeiro.com respeitar 
-                a sua privacidade em relação a qualquer informação sua que possamos coletar no site <Link to='/'>wesinribeiro.com</Link>, e outros sites que possuímos 
+                <p>A sua privacidade é importante para nós. É política do engmoderno.com.br respeitar 
+                a sua privacidade em relação a qualquer informação sua que possamos coletar no site <Link to='/'>engmoderno.com.br</Link>, e outros sites que possuímos 
                 e operamos.</p>                    
                 <p>Solicitamos informações pessoais apenas quando realmente precisamos delas para lhe 
                 fornecer um serviço. Fazemo-lo por meios justos e legais, com o seu conhecimento e 
@@ -37,7 +37,7 @@ const PolicyPage = () => {
                 <p>O uso continuado de nosso site será considerado como aceitação de nossas práticas 
                 em torno de privacidade e informações pessoais. Se você tiver alguma dúvida sobre como 
                 lidamos com dados do usuário e informações pessoais, entre em contacto connosco.</p>
-                <h2>Política de Cookies wesinribeiro.com</h2>
+                <h2>Política de Cookies engmoderno.com.br</h2>
                 <h3>O que são cookies?</h3>
                 <p>Como é prática comum em quase todos os sites profissionais, este site usa cookies, 
                 que são pequenos arquivos baixados no seu computador, para melhorar sua experiência. 
@@ -114,12 +114,12 @@ const PolicyPage = () => {
                 o melhor preço possível.</li> </ul>
                 <h3>Compromisso do Usuário</h3>
                 <p>O usuário se compromete a fazer uso adequado dos conteúdos e da informação que o 
-                wesinribeiro.com oferece no site e com caráter enunciativo, mas não limitativo:</p>
+                engmoderno.com.br oferece no site e com caráter enunciativo, mas não limitativo:</p>
                 <ul><li>A) Não se envolver em atividades que sejam ilegais ou contrárias à boa fé a à 
                 ordem pública;</li>
                 <li>B) Não difundir propaganda ou conteúdo de natureza racista, xenofóbica, 
                 qualquer tipo de pornografia ilegal, de apologia ao terrorismo ou contra os direitos humanos;</li> 
-                <li>C) Não causar danos aos sistemas físicos (hardwares) e lógicos (softwares) do wesinribeiro.com, 
+                <li>C) Não causar danos aos sistemas físicos (hardwares) e lógicos (softwares) do engmoderno.com.br, 
                 de seus fornecedores ou terceiros, para introduzir ou disseminar vírus informáticos ou quaisquer 
                 outros sistemas de hardware ou software que sejam capazes de causar danos anteriormente mencionados.</li>
                 </ul>
--- src/pages/terms.js
+++ src/pages/terms.js
@@ -17,61 +17,61 @@ const TermPage = () => {
                 <Header name='Termos' links={[{path: '/', description:'Home'}]}/>
                 <Box>
                     <h2>1. Termos</h2>            
-                    <p>Ao acessar ao site <Link to='/'>wesinribeiro.com</Link>, concorda em cumprir estes 
+                    <p>Ao acessar ao site <Link to='/'>engmoderno.com.br</Link>, concorda em cumprir estes 
                     termos de serviço, todas as leis e regulamentos aplicáveis ​​e concorda que é responsável pelo 
                     cumprimento de todas as leis locais aplicáveis. Se você não concordar com algum desses termos, 
                     está proibido de usar ou acessar este site. Os materiais contidos neste site são protegidos pelas 
                     leis de direitos autorais e marcas comerciais aplicáveis.</p>            
                     <h2>2. Uso de Licença</h2>            
                     <p>É concedida permissão para baixar temporariamente uma cópia dos materiais 
-                    (informações ou software) no site wesinribeiro.com , apenas para visualização transitória pessoal 
+                    (informações ou software) no site engmoderno.com.br , apenas para visualização transitória pessoal 
                     e não comercial. Esta é a concessão de uma licença, não uma transferência de título e,                
                     sob esta licença, você não pode: </p> 
                     <ol> <li>modificar ou copiar os materiais;  </li>            
                     <li>usar os materiais para qualquer finalidade comercial ou para exibição pública 
                     (comercial ou não comercial);  </li>            
                     <li>tentar descompilar ou fazer engenharia reversa de qualquer software contido no site 
-                    wesinribeiro.com;  </li>            
+                    engmoderno.com.br;  </li>            
                     <li>remover quaisquer direitos autorais ou outras notações de propriedade dos materiais; ou  </li> 
                     <li>transferir os materiais para outra pessoa ou 'espelhe' os materiais em qualquer outro servidor.</li>            
                     </ol>            
                     <p>Esta licença será automaticamente rescindida se você violar alguma dessas restrições e 
-                    poderá ser rescindida por wesinribeiro.com a qualquer momento. Ao encerrar a visualização desses 
+                    poderá ser rescindida por engmoderno.com.br a qualquer momento. Ao encerrar a visualização desses 
                     materiais ou após o término desta licença, você deve apagar todos os materiais                
                     baixados em sua posse, seja em formato eletrónico ou impresso.</p>            
                     <h2>3. Isenção de responsabilidade</h2>            
-                    <ol> <li>Os materiais no site da wesinribeiro.com são fornecidos 'como estão'. wesinribeiro.com 
+                    <ol> <li>Os materiais no site da engmoderno.com.br são fornecidos 'como estão'. engmoderno.com.br 
                     não oferece garantias, expressas ou implícitas, e, por este meio, isenta e nega todas as outras 
                     garantias, incluindo, sem limitação, garantias implícitas ou condições de comercialização,            
                     adequação a um fim específico ou não violação de propriedade intelectual ou outra violação de 
                     direitos. </li>            
-                    <li>Além disso, o wesinribeiro.com não garante ou faz qualquer representação relativa à precisão, 
+                    <li>Além disso, o engmoderno.com.br não garante ou faz qualquer representação relativa à precisão, 
                     aos resultados prováveis ​​ou à confiabilidade do uso dos materiais em seu site ou de 
                     outra forma relacionado a esses materiais ou em sites vinculados a este site.</li> </ol>            
                     <h2>4. Limitações</h2>            
-                    <p>Em nenhum caso o wesinribeiro.com ou seus fornecedores serão responsáveis ​​por quaisquer danos 
+                    <p>Em nenhum caso o engmoderno.com.br ou seus fornecedores serão responsáveis ​​por quaisquer danos 
                     (incluindo, sem limitação, danos por perda de dados ou lucro ou devido a interrupção dos negócios) 
-                    decorrentes do uso ou da incapacidade de usar os materiais em wesinribeiro.com,                
-                    mesmo que wesinribeiro.com ou um representante autorizado da wesinribeiro.com tenha sido notificado 
+                    decorrentes do uso ou da incapacidade de usar os materiais em engmoderno.com.br,                
+                    mesmo que engmoderno.com.br ou um representante autorizado da engmoderno.com.br tenha sido notificado 
                     oralmente ou por escrito da possibilidade de tais danos. Como algumas jurisdições não permitem 
                     limitações em garantias implícitas, ou limitações de responsabilidade                
                     por danos conseqüentes ou incidentais, essas limitações podem não se aplicar a você.</p>            
                     <h2>5. Precisão dos materiais</h2>            
-                    <p>Os materiais exibidos no site da wesinribeiro.com podem incluir erros técnicos, tipográficos ou 
-                    fotográficos. wesinribeiro.com não garante que qualquer material em seu site seja preciso, 
-                    completo ou atual. wesinribeiro.com pode fazer alterações nos materiais contidos em seu 
-                    site a qualquer momento, sem aviso prévio. No entanto, wesinribeiro.com não se compromete a 
+                    <p>Os materiais exibidos no site da engmoderno.com.br podem incluir erros técnicos, tipográficos ou 
+                    fotográficos. engmoderno.com.br não garante que qualquer material em seu site seja preciso, 
+                    completo ou atual. engmoderno.com.br pode fazer alterações nos materiais contidos em seu 
+                    site a qualquer momento, sem aviso prévio. No entanto, engmoderno.com.br não se compromete a 
                     atualizar os materiais.</p>            
                     <h2>6. Links</h2>            
-                    <p>O wesinribeiro.com não analisou todos os sites vinculados ao seu site e não é responsável 
+                    <p>O engmoderno.com.br não analisou todos os sites vinculados ao seu site e não é responsável 
                     pelo conteúdo de nenhum site vinculado. A inclusão de qualquer link não implica endosso por 
-                    wesinribeiro.com do site. O uso de qualquer site vinculado é por conta e risco do usuário.</p>
+                    engmoderno.com.br do site. O uso de qualquer site vinculado é por conta e risco do usuário.</p>
                     <h3>Modificações</h3>            
-                    <p>O wesinribeiro.com pode revisar estes termos de serviço do site a qualquer momento, 
+                    <p>O engmoderno.com.br pode revisar estes termos de serviço do site a qualquer momento, 
                     sem aviso prévio. Ao usar este site, você concorda em ficar vinculado à versão atual desses 
                     termos de serviço.</p>            
                     <h3>Lei aplicável</h3>            
-                    <p>Estes termos e condições são regidos e interpretados de acordo com as leis do wesinribeiro.com 
+                    <p>Estes termos e condições são regidos e interpretados de acordo com as leis do engmoderno.com.br 
                     e você se submete irrevogavelmente à jurisdição exclusiva dos tribunais naquele estado ou 
                     localidade.</p>
                 </Box>                

48615f3476572ef77ee0f62edf860aba3b0d1dd6
Wesin Alves <wesinec@gmail.com>
2024-09-23 08:11:11 -0300

    change video

--- src/pages/index.js
+++ src/pages/index.js
@@ -111,7 +111,7 @@ const IndexPage = () => {
       {/* Hero unit */}
       <Container disableGutters maxWidth="md" component="main" sx={{ pt: 4, pb: 6, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
           
-          <YoutubeEmbed embedId='5PepfmvEgM8' />
+          <YoutubeEmbed embedId='TNn5FJfrktA' />
           <Grid container justifyContent='center' sx={{ marginTop: 1}} spacing={2}>
             <Grid item >                
                 <Button fullwidth variant="contained" color="primary" href="https://forms.gle/qxgNetg74C8XwtU17">

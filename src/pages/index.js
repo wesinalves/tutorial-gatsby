9995013a733e7f9be05001285988a39fd28ebdd3
Wesin Alves <wesinec@gmail.com>
2024-10-11 07:12:39 -0300

    update index with instagram component

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,108 +5,21 @@ import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
-import CardHeader from '@mui/material/CardHeader';
 import CardMedia from '@mui/material/CardMedia';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-import Rating from '@mui/material/Rating';
 import Seo from '../components/seo';
 import { navigate } from "gatsby"
 import YoutubeEmbed from '../components/youtube';
-
-const tiers = [
-  {
-      title: 'Linguagens de Programação',
-      price: '0',
-      description: [
-      ['Python', 5],
-      ['Javascript', 5],
-      ['Php', 5],
-      ['Java', 4.5],
-      ],
-      buttonText: 'Sign up for free',
-      buttonVariant: 'outlined',
-  },
-  {
-      title: 'Aprendizado de Máquina',
-      subheader: 'Most popular',
-      price: '15',
-      description: [
-      ['Keras', 5],
-      ['Tensorflow', 4.5],
-      ['Weka', 5],
-      ['Elki', 4.5],
-      ],
-      buttonText: 'Get started',
-      buttonVariant: 'contained',
-  },
-  {
-      title: 'Tencologias para Devops',
-      price: '30',
-      description: [
-      ['Terraform', 4.5],
-      ['Docker', 5],
-      ['Gitlab CI', 4],
-      ['Kubernetes', 3.5],
-      ],
-      buttonText: 'Contact us',
-      buttonVariant: 'outlined',
-  },
-];
-
-const portfolios = [
-    {
-        title: 'Gestaosecom',
-        description: 'Sistema de controle de propagandas do governo federal',
-        url: 'https://gestaosecom.mcom.gov.br/',
-        image: '../images/gestaosecom.png'
-    },
-    {
-        title: 'Secomweb',
-        description: 'Sistema de publicidade do governo federal',
-        url: 'https://secomweb.mcom.gov.br/',
-        image: '../images/secomweb.png'
-    },
-    {
-        title: 'PGD',
-        description: 'Sistema de gestao de demandas do governo federal',
-        url: 'https://pg.mcom.gov.br/',
-        image: '../images/pgmcom.png'
-    },
-    {
-        title: 'BVS - IEC',
-        description: 'Portal da biblioteca virtual do Instituto Evandro Chagas',
-        url: 'https://bvs.iec.gov.br/iec/',
-        image: '../images/bvsiec.png'
-    },
-    {
-        title: 'Revista RPAS',
-        description: 'Portal da Revista Pan Amazônica de Saúde',
-        url: 'http://revista.iec.gov.br',
-        image: '../images/revistaiec.png'
-    },
-    {
-        title: 'OJS - RPAS',
-        description: 'Sistema de publicações da Revista Pan Amazônica de Saúde',
-        url: 'http://bvs.iec.gov.br/iec/',
-        image: '../images/revistasistema.png'
-    },
-    {
-        title: 'Patuá - IEC',
-        description: 'Repositório digital do Instituto Evandro Chagas',
-        url: 'http://revista.iec.gov.br/submit/index.php/rpas',
-        image: '../images/patua.png'
-    },
-    
-
-]
+import InstagramEmbed from '../components/instagram';
+import { instaPhoto } from '../components/layout.module.css' 
 
 const IndexPage = () => {
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
-    fetch('https://graph.instagram.com/{user-id}/media?fields=id,caption,media_type,media_url,permalink&access_token=YOUR_ACCESS_TOKEN&limit=3')
+    fetch(`https://graph.instagram.com/${process.env.APP_ID}/media?fields=id,caption,media_type,media_url,permalink&access_token=${process.env.ACCESS_TOKEN}&limit=6`)
       .then((response) => response.json())
       .then((data) => setPosts(data.data));
   }, []);
@@ -151,18 +64,18 @@ const IndexPage = () => {
       {/* End hero unit */}
       
       <Container maxWidth="md" component="main">
-      {/* <Typography
+        <Typography
           component="h1"
           variant="h4"          
           align="center"
           color="text.primary"
-          mt={10}
+          mt={0}
           gutterBottom
           >
           Instagram
-          </Typography> */}
+          </Typography>
           <Grid container spacing={5} alignItems="flex-end">
-          {posts.map((post) => (
+          {posts && posts.map((post) => (
               // Enterprise card is full width at sm breakpoint
               <Grid
               item
@@ -172,124 +85,31 @@ const IndexPage = () => {
               md={4}
               >
               <Card >
-                <CardMedia>
+                <CardMedia sx={{backgroundColor:'common.black'}}>
                     {/* <StaticImage
                         alt={portfolio.title}
                         src={portfolio.image}
-                    /> */}
-                    <img src={post.media_url} alt={post.caption} width="100"/>
+                        /> */}
+                    {post.media_type === 'VIDEO'
+                        ? <InstagramEmbed url={post.media_url} />
+                        : <img src={post.media_url} alt={post.caption} className={instaPhoto}/>
+                    }
                 </CardMedia>
                     
                 <CardContent>
-                    <a href={post.permalink} target="_blank" rel="noopener noreferrer">
-                        <Typography variant="body2" color="text.secondary">
-                        {post.caption.substring(0, 30)}
-                        </Typography>
-                    </a>
-                </CardContent>
-                </Card>
-              </Grid>
-          ))}
-          </Grid>
-          <Typography
-          component="h1"
-          variant="h4"          
-          align="center"
-          color="text.primary"
-          mt={2}
-          gutterBottom
-          >
-          Habilidades
-          </Typography>
-          <Grid container spacing={5} alignItems="flex-end">
-          {tiers.map((tier) => (
-              // Enterprise card is full width at sm breakpoint
-              <Grid
-              item
-              key={tier.title}
-              xs={12}
-              sm={tier.title === 'Enterprise' ? 12 : 6}
-              md={4}
-              >
-              <Card>
-                  <CardHeader
-                  title={tier.title}
-                  //subheader={tier.subheader}
-                  titleTypographyProps={{ align: 'center' }}
-                  /*action={tier.title === 'Pro' ? <StarIcon /> : null}*/
-                  subheaderTypographyProps={{
-                      align: 'center',
-                  }}
-                  sx={{
-                      backgroundColor: (theme) =>
-                      theme.palette.mode === 'light'
-                          ? theme.palette.grey[200]
-                          : theme.palette.grey[700],
-                  }}
-                  />
-                  <CardContent>                  
-                  <ul>
-                      {tier.description.map((line) => (
-                      <Typography
-                          component="li"
-                          variant="subtitle1"                          
-                          key={line[0]}
-                          sx={{display: 'flex', justifyContent: 'space-between'}}                          
-                      >
-                          {line[0]}
-                          <Rating name="half-rating-read" defaultValue={line[1]} precision={0.5} readOnly /> 
-                      </Typography>
-                      ))}
-                  </ul>
-                  </CardContent>                  
-              </Card>
-              </Grid>
-          ))}
-          </Grid>
-          <Typography
-          component="h1"
-          variant="h4"          
-          align="center"
-          color="text.primary"
-          mt={10}
-          gutterBottom
-          >
-          Portfolio
-          </Typography>
-          <Grid container spacing={5} alignItems="flex-end">
-          {portfolios.map((portfolio) => (
-              // Enterprise card is full width at sm breakpoint
-              <Grid
-              item
-              key={portfolio.title}
-              xs={12}
-              sm={portfolio.title === 'Enterprise' ? 12 : 6}
-              md={4}
-              >
-              <Card >
-                <CardMedia>
-                    {/* <StaticImage
-                        alt={portfolio.title}
-                        src={portfolio.image}
-                    /> */}
-                </CardMedia>
-                    
-                <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                    {portfolio.title}
-                    </Typography>
                     <Typography variant="body2" color="text.secondary">
-                    {portfolio.description}
+                        {post.caption.substring(0, 50)}...
                     </Typography>
                 </CardContent>
                 <CardActions>
                     <Button size="small" variant="contained" fullWidth 
-                    href={portfolio.url} target="_blank">Acessar</Button>                    
+                    href={post.permalink} target="_blank">Acessar</Button>                    
                 </CardActions>
                 </Card>
               </Grid>
           ))}
           </Grid>
+          
       </Container>
       
       {/* <StaticImage

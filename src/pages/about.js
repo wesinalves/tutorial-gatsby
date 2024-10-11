4186fd57d0ba2164d378e6abcf82fc954c90354d
Wesin Alves <wesinec@gmail.com>
2024-10-11 07:11:24 -0300

    update page about with skills

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,6 +9,51 @@ import AccordionDetails from '@mui/material/AccordionDetails';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import Box from '@mui/material/Box';
 import Seo from '../components/seo';
+import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Rating from '@mui/material/Rating';
+import CardHeader from '@mui/material/CardHeader';
+
+const tiers = [
+    {
+        title: 'Linguagens de Programação',
+        price: '0',
+        description: [
+        ['Python', 5],
+        ['Javascript', 5],
+        ['Php', 5],
+        ['Java', 4.5],
+        ],
+        buttonText: 'Sign up for free',
+        buttonVariant: 'outlined',
+    },
+    {
+        title: 'Aprendizado de Máquina',
+        subheader: 'Most popular',
+        price: '15',
+        description: [
+        ['Keras', 5],
+        ['Tensorflow', 4.5],
+        ['Weka', 5],
+        ['Elki', 4.5],
+        ],
+        buttonText: 'Get started',
+        buttonVariant: 'contained',
+    },
+    {
+        title: 'Tencologias para Devops',
+        price: '30',
+        description: [
+        ['Terraform', 4.5],
+        ['Docker', 5],
+        ['Gitlab CI', 4],
+        ['Kubernetes', 3.5],
+        ],
+        buttonText: 'Contact us',
+        buttonVariant: 'outlined',
+    },
+  ];
 
 const AboutPage = () => {
     return (
@@ -86,7 +131,64 @@ const AboutPage = () => {
                         </AccordionDetails>
                     </Accordion>
                 </Box>
+
+                <Typography
+          component="h1"
+          variant="h4"          
+          align="center"
+          color="text.primary"
+          mt={2}
+          gutterBottom
+          >
+          Habilidades
+          </Typography>
+          <Grid container spacing={5} alignItems="flex-end">
+          {tiers.map((tier) => (
+              // Enterprise card is full width at sm breakpoint
+              <Grid
+              item
+              key={tier.title}
+              xs={12}
+              sm={tier.title === 'Enterprise' ? 12 : 6}
+              md={4}
+              >
+              <Card>
+                  <CardHeader
+                  title={tier.title}
+                  //subheader={tier.subheader}
+                  titleTypographyProps={{ align: 'center' }}
+                  /*action={tier.title === 'Pro' ? <StarIcon /> : null}*/
+                  subheaderTypographyProps={{
+                      align: 'center',
+                  }}
+                  sx={{
+                      backgroundColor: (theme) =>
+                      theme.palette.mode === 'light'
+                          ? theme.palette.grey[200]
+                          : theme.palette.grey[700],
+                  }}
+                  />
+                  <CardContent>                  
+                  <ul>
+                      {tier.description.map((line) => (
+                      <Typography
+                          component="li"
+                          variant="subtitle1"                          
+                          key={line[0]}
+                          sx={{display: 'flex', justifyContent: 'space-between'}}                          
+                      >
+                          {line[0]}
+                          <Rating name="half-rating-read" defaultValue={line[1]} precision={0.5} readOnly /> 
+                      </Typography>
+                      ))}
+                  </ul>
+                  </CardContent>                  
+              </Card>
+              </Grid>
+          ))}
+          </Grid>
             </Container>
+            
             
         </Layout>
     )

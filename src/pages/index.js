20e12d571d20c841537158d68c2b390a5826ad9a
Wesin Alves <wesinec@gmail.com>
2025-01-28 21:52:42 -0300

    update video

--- src/pages/index.js
+++ src/pages/index.js
@@ -32,8 +32,7 @@ const IndexPage = () => {
         />
       {/* Hero unit */}
       <Container disableGutters maxWidth="md" component="main" sx={{ pt: 4, pb: 6, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-          
-          <YoutubeEmbed embedId='V0OCHuxQaSk' />
+          <YoutubeEmbed embedId='_3kvTnE2qtc' />
           <Grid container justifyContent='center' sx={{ marginTop: 1}} spacing={2}>
             <Grid item >                
                 <Button fullwidth variant="contained" color="primary" href="https://forms.gle/qxgNetg74C8XwtU17">

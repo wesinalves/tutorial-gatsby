b560948532d65ef6f6ec17d1425cca4873cea9fc
Wesin Alves <wesinec@gmail.com>
2026-08-16 21:03:10 -0300

    adiciona novo vídeo, corrige instagram

--- src/pages/index.js
+++ src/pages/index.js
@@ -33,7 +33,7 @@ const IndexPage = () => {
         />
       {/* Hero unit */}
       <Container disableGutters maxWidth="md" component="main" sx={{ pt: 4, pb: 6, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-          <YoutubeEmbed embedId='pBdxT2cwie8?si=taDceF_vi3NrcDCJ' />
+          <YoutubeEmbed embedId='n1FyaX6skqk?si=l1IQ_xk5uL8XIOay' />
           <Grid container justifyContent='center' sx={{ marginTop: 1}} spacing={2}>
             <Grid item >                
                 <Button fullwidth variant="contained" color="primary" href="https://forms.gle/qxgNetg74C8XwtU17">
